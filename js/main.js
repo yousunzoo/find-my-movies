f47505bcd9968cb0bb787a0bd53b1997d7be228f
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-04 12:09:09 +0900

    feat: search option setting
    - 현재 연도로부터 50년 이전까지 옵션 불러오기

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+// 페이지 접속 시 배너 클래스 추가
 const bannerEl = document.querySelector(".banner");
 
 window.onload = function () {
   bannerEl.classList.add("loaded");
 };
+
+// select 옵션에 현재 연도로부터 50년 이전까지 연도 별 option 넣기
+const yearsEl = document.querySelector("#years");
+const year = new Date().getFullYear();
+
+for (let i = year; i >= year - 50; i--) {
+  let yearEl = document.createElement("option");
+  yearEl.value = i;
+  yearEl.textContent = i;
+  yearsEl.appendChild(yearEl);
+}

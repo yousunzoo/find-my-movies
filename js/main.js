a2bddc6aceeab7a8732c24253eb6225758831fda
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-05 17:05:23 +0900

    feat: 검색결과 갯수 출력

--- js/main.js
+++ js/main.js
@@ -16,22 +16,51 @@ for (let i = year; i >= year - 50; i--) {
   yearsEl.appendChild(yearEl);
 }
 
-// search 버튼 누르면 API 받아오기
-
 const inputEl = document.querySelector("input");
 const selectEl = document.querySelector("select");
 const SearchEl = document.querySelector(".search__btn");
+
+// search 버튼 누르면 inputEl, selectEl value 가져와서 getMovies에 넣기
 SearchEl.addEventListener("click", function (e) {
   e.preventDefault();
+  const title = inputEl.value;
+  const year = selectEl.value;
+  getMovies(title, year, true);
 });
 
 // 영화 API 받아오기
-async function getMovies(title, year) {
-  const s = `$s=${title}`;
-  const y = `$y=${year}`;
-  await fetch(`https://omdbapi.com/?apikey=7035c60c${s}${y}`)
+
+let movies = [];
+let totalResults;
+
+async function getMovies(title, year, isFirst) {
+  const s = `&s=${title}`;
+  const y = year === "All Years" ? "" : `&y=${year}`;
+  await fetch(`https://www.omdbapi.com/?apikey=d2f6bdf1${s}${y}`)
     .then((response) => {
       return response.json();
     })
-    .then((data) => console.log(data));
+    .then((data) => {
+      if (data.Response === "True") {
+        movies = data.Search;
+        totalResults = data.totalResults;
+        setMovies(isFirst);
+      }
+      return data.Error;
+    });
+}
+
+// 받아온 JSON 데이터 출력
+const resultDiv = document.querySelector(".result");
+
+function setMovies(isFirst) {
+  const totalEl = document.createElement("p");
+  totalEl.innerHTML = `총 <span>${totalResults}</span>개의 검색결과가 있습니다.`;
+  totalEl.classList.add("total");
+
+  if (isFirst) {
+    resultDiv.innerHTML = "";
+  }
+
+  resultDiv.append(totalEl);
 }
